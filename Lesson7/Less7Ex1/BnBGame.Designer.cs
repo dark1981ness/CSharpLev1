2db1be5846658093506d106badf6e4b95e5ea3a8
Fenist <kostik06@gmail.com>
2020-10-20 18:24:50 +0300

    123

--- Lesson7/Less7Ex1/BnBGame.Designer.cs
+++ Lesson7/Less7Ex1/BnBGame.Designer.cs
@@ -30,16 +30,17 @@
         {
             this.lblHeader = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblQuestion = new System.Windows.Forms.Label();
-            this.btnTrue = new System.Windows.Forms.Button();
-            this.btnFalse = new System.Windows.Forms.Button();
-            this.lblCompareResult = new System.Windows.Forms.Label();
-            this.lblTrueAnswerCount = new System.Windows.Forms.Label();
-            this.lblFalseAnswerCount = new System.Windows.Forms.Label();
-            this.lblTrueCount = new System.Windows.Forms.Label();
-            this.lblFalseCount = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFalseCount = new System.Windows.Forms.Label();
+            this.lblTrueCount = new System.Windows.Forms.Label();
+            this.lblFalseAnswerCount = new System.Windows.Forms.Label();
+            this.lblTrueAnswerCount = new System.Windows.Forms.Label();
+            this.lblCompareResult = new System.Windows.Forms.Label();
+            this.btnFalse = new System.Windows.Forms.Button();
+            this.btnTrue = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnStartGame = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -75,45 +76,77 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(481, 495);
             this.panel1.TabIndex = 1;
+            this.panel1.Visible = false;
             // 
-            // lblQuestion
+            // label2
             // 
-            this.lblQuestion.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblQuestion.AutoSize = true;
-            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblQuestion.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblQuestion.Location = new System.Drawing.Point(11, 81);
-            this.lblQuestion.Name = "lblQuestion";
-            this.lblQuestion.Size = new System.Drawing.Size(0, 20);
-            this.lblQuestion.TabIndex = 1;
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(233, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "1 из 20";
             // 
-            // btnTrue
+            // label1
             // 
-            this.btnTrue.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnTrue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
-            this.btnTrue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTrue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnTrue.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnTrue.Location = new System.Drawing.Point(72, 243);
-            this.btnTrue.Name = "btnTrue";
-            this.btnTrue.Size = new System.Drawing.Size(90, 35);
-            this.btnTrue.TabIndex = 2;
-            this.btnTrue.Text = "Верю!";
-            this.btnTrue.UseVisualStyleBackColor = false;
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(163, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Вопрос";
             // 
-            // btnFalse
+            // lblFalseCount
             // 
-            this.btnFalse.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnFalse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
-            this.btnFalse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFalse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnFalse.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnFalse.Location = new System.Drawing.Point(301, 243);
-            this.btnFalse.Name = "btnFalse";
-            this.btnFalse.Size = new System.Drawing.Size(90, 35);
-            this.btnFalse.TabIndex = 3;
-            this.btnFalse.Text = "Не верю!";
-            this.btnFalse.UseVisualStyleBackColor = false;
+            this.lblFalseCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblFalseCount.AutoSize = true;
+            this.lblFalseCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblFalseCount.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblFalseCount.Location = new System.Drawing.Point(286, 353);
+            this.lblFalseCount.Name = "lblFalseCount";
+            this.lblFalseCount.Size = new System.Drawing.Size(0, 20);
+            this.lblFalseCount.TabIndex = 16;
+            // 
+            // lblTrueCount
+            // 
+            this.lblTrueCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTrueCount.AutoSize = true;
+            this.lblTrueCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTrueCount.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTrueCount.Location = new System.Drawing.Point(286, 316);
+            this.lblTrueCount.Name = "lblTrueCount";
+            this.lblTrueCount.Size = new System.Drawing.Size(0, 20);
+            this.lblTrueCount.TabIndex = 15;
+            // 
+            // lblFalseAnswerCount
+            // 
+            this.lblFalseAnswerCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblFalseAnswerCount.AutoSize = true;
+            this.lblFalseAnswerCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblFalseAnswerCount.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblFalseAnswerCount.Location = new System.Drawing.Point(11, 353);
+            this.lblFalseAnswerCount.Name = "lblFalseAnswerCount";
+            this.lblFalseAnswerCount.Size = new System.Drawing.Size(247, 20);
+            this.lblFalseAnswerCount.TabIndex = 14;
+            this.lblFalseAnswerCount.Text = "Количество неверных ответов:";
+            // 
+            // lblTrueAnswerCount
+            // 
+            this.lblTrueAnswerCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTrueAnswerCount.AutoSize = true;
+            this.lblTrueAnswerCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTrueAnswerCount.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTrueAnswerCount.Location = new System.Drawing.Point(11, 316);
+            this.lblTrueAnswerCount.Name = "lblTrueAnswerCount";
+            this.lblTrueAnswerCount.Size = new System.Drawing.Size(229, 20);
+            this.lblTrueAnswerCount.TabIndex = 13;
+            this.lblTrueAnswerCount.Text = "Количество верных ответов:";
             // 
             // lblCompareResult
             // 
@@ -128,75 +161,61 @@
             this.lblCompareResult.Text = "?";
             this.lblCompareResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // lblTrueAnswerCount
+            // btnFalse
             // 
-            this.lblTrueAnswerCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblTrueAnswerCount.AutoSize = true;
-            this.lblTrueAnswerCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTrueAnswerCount.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblTrueAnswerCount.Location = new System.Drawing.Point(11, 316);
-            this.lblTrueAnswerCount.Name = "lblTrueAnswerCount";
-            this.lblTrueAnswerCount.Size = new System.Drawing.Size(229, 20);
-            this.lblTrueAnswerCount.TabIndex = 13;
-            this.lblTrueAnswerCount.Text = "Количество верных ответов:";
+            this.btnFalse.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnFalse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.btnFalse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFalse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFalse.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnFalse.Location = new System.Drawing.Point(301, 243);
+            this.btnFalse.Name = "btnFalse";
+            this.btnFalse.Size = new System.Drawing.Size(90, 35);
+            this.btnFalse.TabIndex = 3;
+            this.btnFalse.Text = "Не верю!";
+            this.btnFalse.UseVisualStyleBackColor = false;
+            this.btnFalse.Click += new System.EventHandler(this.btnFalse_Click);
             // 
-            // lblFalseAnswerCount
+            // btnTrue
             // 
-            this.lblFalseAnswerCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblFalseAnswerCount.AutoSize = true;
-            this.lblFalseAnswerCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblFalseAnswerCount.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblFalseAnswerCount.Location = new System.Drawing.Point(11, 353);
-            this.lblFalseAnswerCount.Name = "lblFalseAnswerCount";
-            this.lblFalseAnswerCount.Size = new System.Drawing.Size(247, 20);
-            this.lblFalseAnswerCount.TabIndex = 14;
-            this.lblFalseAnswerCount.Text = "Количество неверных ответов:";
+            this.btnTrue.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnTrue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.btnTrue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTrue.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnTrue.Location = new System.Drawing.Point(72, 243);
+            this.btnTrue.Name = "btnTrue";
+            this.btnTrue.Size = new System.Drawing.Size(90, 35);
+            this.btnTrue.TabIndex = 2;
+            this.btnTrue.Text = "Верю!";
+            this.btnTrue.UseVisualStyleBackColor = false;
+            this.btnTrue.Click += new System.EventHandler(this.btnTrue_Click);
             // 
-            // lblTrueCount
+            // lblQuestion
             // 
-            this.lblTrueCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblTrueCount.AutoSize = true;
-            this.lblTrueCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTrueCount.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblTrueCount.Location = new System.Drawing.Point(286, 316);
-            this.lblTrueCount.Name = "lblTrueCount";
-            this.lblTrueCount.Size = new System.Drawing.Size(0, 20);
-            this.lblTrueCount.TabIndex = 15;
+            this.lblQuestion.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuestion.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblQuestion.Location = new System.Drawing.Point(11, 81);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(0, 20);
+            this.lblQuestion.TabIndex = 1;
             // 
-            // lblFalseCount
+            // btnStartGame
             // 
-            this.lblFalseCount.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblFalseCount.AutoSize = true;
-            this.lblFalseCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblFalseCount.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblFalseCount.Location = new System.Drawing.Point(286, 353);
-            this.lblFalseCount.Name = "lblFalseCount";
-            this.lblFalseCount.Size = new System.Drawing.Size(0, 20);
-            this.lblFalseCount.TabIndex = 16;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(163, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
-            this.label1.TabIndex = 17;
-            this.label1.Text = "Вопрос";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(233, 42);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 20);
-            this.label2.TabIndex = 18;
-            this.label2.Text = "1 из 20";
+            this.btnStartGame.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnStartGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.btnStartGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStartGame.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnStartGame.Location = new System.Drawing.Point(186, 96);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(90, 78);
+            this.btnStartGame.TabIndex = 19;
+            this.btnStartGame.Text = "СТАРТ";
+            this.btnStartGame.UseVisualStyleBackColor = false;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
             // 
             // BnBGame
             // 
@@ -204,6 +223,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
             this.ClientSize = new System.Drawing.Size(481, 496);
+            this.Controls.Add(this.btnStartGame);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "BnBGame";
@@ -228,5 +248,6 @@
         private System.Windows.Forms.Label lblTrueAnswerCount;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartGame;
     }
 }
--- Lesson7/Less7Ex1/BnBGame.cs
+++ Lesson7/Less7Ex1/BnBGame.cs
@@ -12,9 +12,76 @@ namespace Less7Ex1
 {
     public partial class BnBGame : Form
     {
+        TrueFalse gameDB;
+
+        public int QuestionIndex { get; set; } = 1;
+        public int DBIndex { get; set; }
+        public int GameDBCount { get; set; }
+        public bool UserAnswer { get; set; }
+        public bool BaseAnswer { get; set; }
+        public int RightAnswersCount { get; set; } = 0;
+        public int WrongAnswersCount { get; set; } = 0;
+
         public BnBGame()
         {
             InitializeComponent();
         }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            
+            panel1.Visible = true;
+            btnStartGame.Visible = false;
+            LoadGameBase();
+            GameDBCount = gameDB.Count;
+            GetQuestion();
+        }
+
+        private void LoadGameBase()
+        {
+            gameDB = new TrueFalse("myDB");
+            gameDB.Load();
+        }
+
+        private void btnTrue_Click(object sender, EventArgs e)
+        {
+            GetQuestion();
+            UserAnswer = true;
+            CompareAnswers(UserAnswer, BaseAnswer);
+        }
+
+        private void btnFalse_Click(object sender, EventArgs e)
+        {
+            GetQuestion();
+            UserAnswer = false;
+            CompareAnswers(UserAnswer, BaseAnswer);
+            
+        }
+
+        private void GetQuestion()
+        {
+            Random random = new Random();
+            DBIndex = random.Next(0, gameDB.Count);
+            label2.Text = $"{QuestionIndex} из {GameDBCount}";
+            lblQuestion.Text = gameDB[DBIndex].text;
+            BaseAnswer = gameDB[DBIndex].trueFalse;
+            QuestionIndex++;
+            gameDB.Remove(DBIndex);
+        }
+
+        private void CompareAnswers(bool userValue, bool baseValue)
+        {
+            if (userValue == baseValue)
+            {
+                RightAnswersCount++;
+                lblTrueAnswerCount.Text = RightAnswersCount.ToString();
+            }
+            else
+            {
+                WrongAnswersCount++;
+                lblFalseAnswerCount.Text = WrongAnswersCount.ToString();
+            }
+        }
+        
     }
 }
